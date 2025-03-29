2a1c0ad7db19a3e1355d73b7a029b0a785afa639
readhash <dooornew@gmail.com>
2025-03-29 04:20:48 +0000

    Updated Christian section with English labels and branches

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// ...existing code...
+
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    next();
+});
+
+// ...existing code...
